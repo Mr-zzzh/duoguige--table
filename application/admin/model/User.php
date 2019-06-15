3548a3eceaf22b47fcac861c7d972b5377156515
陈 <18627824375@163.com>
2019-06-15 10:40:32 +0800

    后台手机用户详情

--- application/admin/model/User.php
+++ application/admin/model/User.php
@@ -99,12 +99,16 @@ class User extends Common {
             unset($item['salt']);
             unset($item['token']);
             if ($item['type'] == 2) {
-                $check = db()->where()->find();
+                $check = db('technician')->where('uid', $item['id'])->find();
             } elseif ($item['type'] == 3) {
-                $check = db()->where()->find();
+                $check = db('company')->where('uid', $item['id'])->find();
             } else {
                 $check = array();
             }
+            if (!empty($check)) {
+                $check['createtime'] = date('Y-m-d H:i:s', $check['createtime']);
+            }
+            $item['check'] = $check;
         }
         show_json(1, $item);
     }
