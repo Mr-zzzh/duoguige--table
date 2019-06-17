37f7bee6c92c698555c33565769225eacb5c094f
陈 <18627824375@163.com>
2019-06-17 11:29:19 +0800

    商品列表

--- application/mobile/controller/Goods.php
+++ application/mobile/controller/Goods.php
@@ -5,17 +5,29 @@ namespace app\mobile\controller;
 use think\Request;
 
 /**
- * @title Goods
+ * @title 商品管理
  * @group MOBILE
  */
 class Goods extends Common {
 
     /**
-     * @title 列表
+     * @title 商品分类列表
+     * @url /goodscate
+     * @method get
+     * @return data:列表@
+     * @data id:id name:商品名 thumbnail:商品缩略图 price:价格
+     * @author 开发者
+     */
+    public function goodscate() {
+        $m = new \app\mobile\model\Goods();
+        $m->GetAll(request()->get());
+    }
+
+    /**
+     * @title 商品列表
      * @url /goods
      * @method get
-     * @param name:starttime type:string require:0 default:- other:- desc:开始时间(年-月-日_时:分:秒)
-     * @param name:endtime type:string require:0 default:- other:- desc:结束时间(年-月-日_时:分:秒)
+     * @param name:cid type:int require:0 default:- other:- desc:商品分类id
      * @param name:keyword type:string require:0 default:- other:- desc:关键字检索
      * @param name:limit type:int require:0 default:15 desc:每页记录数
      * @param name:page type:int require:0 default:1 desc:获取的页码
@@ -24,88 +36,12 @@ class Goods extends Common {
      * @return current_page:当前的页码
      * @return last_page:最后的页码
      * @return data:列表@
-     * @data id:id name:商品名 bid:品牌id cid:商品分类id thumbnail:商品缩略图 image:详情轮播图 specification:规格 model:型号 manufacturers:厂家名称 phone:销售电话 price:价格 label:标签 intro:详情 address:地址 createtime:创建时间
+     * @data id:id name:商品名 thumbnail:商品缩略图 price:价格
      * @author 开发者
      */
     public function index() {
         $m = new \app\mobile\model\Goods();
         $m->GetAll(request()->get());
-    }
-
-    /**
-     * @title 依赖数据
-     * @url /goods/create
-     * @method get
-     * @return key:value
-     * @author 开发者
-     */
-    public function create() {
-
-    }
-
-    /**
-     * @title 添加
-     * @url /goods
-     * @method post
-     * @param name:name type:string require:1 default:- other:- desc:商品名
-     * @param name:bid type:int require:1 default:- other:- desc:品牌id
-     * @param name:cid type:int require:1 default:- other:- desc:商品分类id
-     * @param name:thumbnail type:string require:1 default:- other:- desc:商品缩略图
-     * @param name:image type:string require:1 default:- other:- desc:详情轮播图
-     * @param name:specification type:string require:1 default:- other:- desc:规格
-     * @param name:model type:string require:1 default:- other:- desc:型号
-     * @param name:manufacturers type:string require:1 default:- other:- desc:厂家名称
-     * @param name:phone type:string require:1 default:- other:- desc:销售电话
-     * @param name:price type:float require:1 default:- other:- desc:价格
-     * @param name:label type:string require:1 default:- other:- desc:标签
-     * @param name:intro type:string require:1 default:- other:- desc:详情
-     * @param name:address type:string require:1 default:- other:- desc:地址
-     * @author 开发者
-     */
-    public function save() {
-        $m = new \app\mobile\model\Goods();
-        $m->AddOne(request()->post());
-    }
-
-    /**
-     * @title 删除
-     * @url /goods/:id
-     * @method delete
-     * @author 开发者
-     */
-    public function delete($id) {
-        if ($id < 1) {
-            show_json(0, '参数ID错误');
-        }
-        $m = new \app\mobile\model\Goods();
-        $m->DelOne($id);
-    }
-
-    /**
-     * @title 编辑
-     * @url /goods/:id
-     * @method put
-     * @param name:name type:string require:1 default:- other:- desc:商品名
-     * @param name:bid type:int require:1 default:- other:- desc:品牌id
-     * @param name:cid type:int require:1 default:- other:- desc:商品分类id
-     * @param name:thumbnail type:string require:1 default:- other:- desc:商品缩略图
-     * @param name:image type:string require:1 default:- other:- desc:详情轮播图
-     * @param name:specification type:string require:1 default:- other:- desc:规格
-     * @param name:model type:string require:1 default:- other:- desc:型号
-     * @param name:manufacturers type:string require:1 default:- other:- desc:厂家名称
-     * @param name:phone type:string require:1 default:- other:- desc:销售电话
-     * @param name:price type:float require:1 default:- other:- desc:价格
-     * @param name:label type:string require:1 default:- other:- desc:标签
-     * @param name:intro type:string require:1 default:- other:- desc:详情
-     * @param name:address type:string require:1 default:- other:- desc:地址
-     * @author 开发者
-     */
-    public function update(Request $request, $id) {
-        if ($id < 1) {
-            show_json(0, '参数ID错误');
-        }
-        $m = new \app\mobile\model\Goods();
-        $m->EditOne($request->put(), $id);
     }
 
     /**
--- application/mobile/model/Goods.php
+++ application/mobile/model/Goods.php
@@ -6,13 +6,13 @@ class Goods extends Common {
 
     public function GetAll($params) {
         $map = array();
-        if (!empty($params['starttime']) && !empty($params['endtime'])) {
-            $map['createtime'] = array('between', strtotime($params['starttime']) . ',' . strtotime($params['endtime']));
-        }
         if (!empty($params['keyword'])) {
-            $map['name|thumbnail|image|specification|model|manufacturers|phone|label|intro|address'] = array('LIKE', '%' . trim($params['keyword']) . '%');
+            $map['name|specification|model|manufacturers|phone|label|intro|area|province|color|address'] = array('LIKE', '%' . trim($params['keyword']) . '%');
         }
-        $list = $this->where($map)->paginate($params['limit'])->toArray();
+        if (!empty($params['cid'])) {
+            $map['cid'] = intval($params['cid']);
+        }
+        $list = $this->field('id,name,thumbnail,price')->where($map)->order('sale_number desc')->paginate($params['limit'])->toArray();
         if (!empty($list['data'])) {
             foreach ($list['data'] as $k => &$item) {
                 $item['createtime'] = date('Y-m-d H:i:s', $item['createtime']);
@@ -22,76 +22,12 @@ class Goods extends Common {
         show_json(1, $list);
     }
 
-    public function AddOne($params) {
-        $data = array(
-            'name' => trim($params['name']),
-            'bid' => intval($params['bid']),
-            'cid' => intval($params['cid']),
-            'thumbnail' => trim($params['thumbnail']),
-            'image' => trim($params['image']),
-            'specification' => trim($params['specification']),
-            'model' => trim($params['model']),
-            'manufacturers' => trim($params['manufacturers']),
-            'phone' => trim($params['phone']),
-            'price' => trim($params['price']) * 1,
-            'label' => trim($params['label']),
-            'intro' => trim($params['intro']),
-            'address' => trim($params['address']),
-            'createtime' => time(),
-        );
-        $this->checkData($data, 0);
-        if ($this->data($data, true)->isUpdate(false)->save()) {
-            //logs('创建新的??,ID:' . $this->getLastInsID(), 1);
-            show_json(1, '添加成功');
-        } else {
-            show_json(0, '添加失败');
-        }
-    }
-
-    private function checkData(&$data, $id = 0) {
-        //TODO 数据校验
-    }
-
-    public function DelOne($id) {
-        if ($this->where(array('id' => $id))->delete()) {
-            //logs('删除??,ID:' . $id, 2);
-            show_json(1, '删除成功');
-        } else {
-            show_json(0, '删除失败');
-        }
-    }
-
-    public function EditOne($params, $id) {
-        $data = array(
-            'name' => trim($params['name']),
-            'bid' => intval($params['bid']),
-            'cid' => intval($params['cid']),
-            'thumbnail' => trim($params['thumbnail']),
-            'image' => trim($params['image']),
-            'specification' => trim($params['specification']),
-            'model' => trim($params['model']),
-            'manufacturers' => trim($params['manufacturers']),
-            'phone' => trim($params['phone']),
-            'price' => trim($params['price']) * 1,
-            'label' => trim($params['label']),
-            'intro' => trim($params['intro']),
-            'address' => trim($params['address']),
-        );
-        $this->checkData($data, $id);
-        if ($this->save($data, array('id' => $id)) !== false) {
-            //logs('编辑??,ID:' . $id, 3);
-            show_json(1, '编辑成功');
-        } else {
-            show_json(0, '编辑失败');
-        }
-    }
-
     public function GetOne($id) {
         $item = $this->get($id);
         if (empty($item)) {
             show_json(1);
         } else {
-            $item = $item->toArray();
+            $item               = $item->toArray();
             $item['createtime'] = date('Y-m-d H:i:s', $item['createtime']);
         }
         show_json(1, $item);
