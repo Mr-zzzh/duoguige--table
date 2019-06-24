85c877b70b1432ae38bf4ce3ec9735036e6490b5
陈 <18627824375@163.com>
2019-06-24 13:55:31 +0800

    发布维保单

--- application/mobile/controller/Maintenance.php
+++ application/mobile/controller/Maintenance.php
@@ -35,27 +35,19 @@ class Maintenance extends Common {
     }
 
     /**
-     * @title 添加
+     * @title 添加(维保单)
      * @url /maintenance
      * @method post
-     * @param name:uid type:int require:1 default:- other:- desc:用户id
+     * @param name:genre type:int require:1 default:- other:- desc:类型_1维修单_2保养单
      * @param name:brand type:string require:1 default:- other:- desc:电梯品牌
      * @param name:model type:string require:1 default:- other:- desc:型号
      * @param name:floor_number type:int require:1 default:- other:- desc:楼层数
      * @param name:type type:string require:1 default:- other:- desc:维修类型
      * @param name:company type:string require:1 default:- other:- desc:单位名称
+     * @param name:province type:int require:1 default:- other:- desc:省编号
+     * @param name:city type:int require:1 default:- other:- desc:市编号
+     * @param name:area type:int require:1 default:- other:- desc:区编号
      * @param name:address type:string require:1 default:- other:- desc:地址
-     * @param name:status type:int require:1 default:- other:- desc:-1取消0待审_1审核通过_2不通过_3已接单_4已完成_5投诉_6投诉已处理
-     * @param name:star type:int require:1 default:- other:- desc:打星
-     * @param name:evaluate type:string require:1 default:- other:- desc:评价
-     * @param name:complain type:string require:1 default:- other:- desc:投诉
-     * @param name:complain_image type:string require:1 default:- other:- desc:投诉照片
-     * @param name:checktime type:int require:1 default:- other:- desc:审核时间
-     * @param name:canceltime type:int require:1 default:- other:- desc:取消时间
-     * @param name:finishtime type:int require:1 default:- other:- desc:完成时间
-     * @param name:evaluate_time type:int require:1 default:- other:- desc:评价时间
-     * @param name:complain_time type:int require:1 default:- other:- desc:投诉时间
-     * @param name:complain_finish_time type:int require:1 default:- other:- desc:投诉完成时间
      * @author 开发者
      */
     public function save() {
--- application/mobile/model/Maintenance.php
+++ application/mobile/model/Maintenance.php
@@ -29,27 +29,55 @@ class Maintenance extends Common {
     }
 
     public function AddOne($params) {
+        global $member;
+        if (check_often(request()->controller() . '_' . request()->action() . '_' . $member['id'])) {
+            show_json(0, '请勿频繁操作');
+        }
         $data = array(
-            'uid'                  => intval($params['uid']),
-            'brand'                => trim($params['brand']),
-            'model'                => trim($params['model']),
-            'floor_number'         => intval($params['floor_number']),
-            'type'                 => trim($params['type']),
-            'company'              => trim($params['company']),
-            'address'              => trim($params['address']),
-            'status'               => intval($params['status']),
-            'star'                 => intval($params['star']),
-            'evaluate'             => trim($params['evaluate']),
-            'complain'             => trim($params['complain']),
-            'complain_image'       => trim($params['complain_image']),
-            'checktime'            => intval($params['checktime']),
-            'canceltime'           => intval($params['canceltime']),
-            'finishtime'           => intval($params['finishtime']),
-            'evaluate_time'        => intval($params['evaluate_time']),
-            'complain_time'        => intval($params['complain_time']),
-            'complain_finish_time' => intval($params['complain_finish_time']),
-            'createtime'           => time(),
+            'uid'          => $member['id'],
+            'brand'        => trim($params['brand']),
+            'model'        => trim($params['model']),
+            'floor_number' => intval($params['floor_number']),
+            'type'         => trim($params['type']),
+            'company'      => trim($params['company']),
+            'province'     => intval($params['province']),
+            'city'         => intval($params['city']),
+            'area'         => intval($params['area']),
+            'address'      => trim($params['address']),
+            'genre'        => intval($params['genre']),
+            'status'       => 0,
+            'createtime'   => time(),
         );
+        if (empty($data['brand'])) {
+            show_json(0, '电梯品牌不能为空');
+        }
+        if (empty($data['model'])) {
+            show_json(0, '电梯型号不能为空');
+        }
+        if (empty($data['floor_number'])) {
+            show_json(0, '楼层数不能为空');
+        }
+        if (empty($data['type'])) {
+            show_json(0, '维修类型不能为空');
+        }
+        if (empty($data['company'])) {
+            show_json(0, '单位名称不能为空');
+        }
+        if (empty($data['province'])) {
+            show_json(0, '省不能为空');
+        }
+        if (empty($data['city'])) {
+            show_json(0, '市不能为空');
+        }
+        if (empty($data['area'])) {
+            show_json(0, '区不能为空');
+        }
+        if (empty($data['address'])) {
+            show_json(0, '详细地址不能为空');
+        }
+        if (empty($data['genre'])) {
+            show_json(0, '维保单类型不能为空');
+        }
         if ($this->data($data, true)->isUpdate(false)->save()) {
             show_json(1, '添加成功');
         } else {
