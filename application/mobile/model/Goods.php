0b24f103b236961ca99b6558651db1b9ebc48777
陈 <18627824375@163.com>
2019-06-26 16:41:27 +0800

    浏览记录

--- application/mobile/model/Goods.php
+++ application/mobile/model/Goods.php
@@ -23,15 +23,32 @@ class Goods extends Common {
     }
 
     public function GetOne($id) {
+        global $member;
         $item = $this->alias('a')
             ->join('brand b', 'a.bid=b.id', 'left')
             ->field('a.*,b.name bname')->where('a.id', $id)->find();
         if (empty($item)) {
             show_json(1);
         } else {
-            $item               = $item->toArray();
-            $item['image']      = explode(',', $item['image']);
-            $item['label']      = explode(',', $item['label']);
+            $item = $item->toArray();
+            if (!empty($item['image'])) {
+                $item['image'] = explode(',', $item['image']);
+            }
+            if (!empty($item['label'])) {
+                $item['label'] = explode(',', $item['label']);
+                if (!empty($member)) {
+                    foreach ($item['label'] as &$v) {
+                        if (db('browse_history')->where(array('uid' => $member['id'], 'lable' => $v))->value('id')) {
+                            db('browse_history')->where(array('uid' => $member['id'], 'lable' => $v))->setInc('number');
+                        } else {
+                            $data['uid']    = $member['id'];
+                            $data['lable']  = $v;
+                            $data['number'] = 1;
+                            db('browse_history')->insert($data);
+                        }
+                    }
+                }
+            }
             $item['createtime'] = date('Y-m-d H:i:s', $item['createtime']);
         }
         show_json(1, $item);
