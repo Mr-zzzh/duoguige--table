9a5e276080604813ed3ee6fbf0df65640885a758
zengzhihang <1095959430@qq.com>
2019-07-04 10:42:53 +0800

    vue基本

--- public_html/yuntiqaq/src/router/index.js
+++ public_html/yuntiqaq/src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+      path: '/register',
+      name: 'register',
+      component: resolve => require(['../components/register.vue'], resolve),
+      meta: {
+        title: '员工首页',
+      },
+    },
+    // {
+    //   path: '/',
+    //   component: resolve => require(['../components/common/Home.vue'], resolve),
+    //   meta: {
+    //     title: '自述文件'
+    //   },
+    //   children: [
+    //     {
+    //       path: '/admin_index',
+    //       name: 'admin_index',
+    //       component: resolve => require(['../components/page/index/index.vue'], resolve),
+    //       meta: {
+    //         title: '首页',
+    //       },
+    //     },
+    //   ]
+    // },
   ]
 })
