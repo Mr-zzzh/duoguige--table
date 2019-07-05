665975cb11b2e04b6fd274eacca9fc2e8f3515ce
陈 <18627824375@163.com>
2019-07-05 10:21:27 +0800

    短信设置添加

--- application/admin/controller/Note.php
+++ application/admin/controller/Note.php
@@ -19,7 +19,7 @@ class Note extends Common {
      * @author 开发者
      */
     public function index() {
-        $m = new \app\admin\model\note();
+        $m = new \app\admin\model\Note();
         $m->GetAll();
     }
 
@@ -27,16 +27,15 @@ class Note extends Common {
      * @title 添加
      * @url /admin/note
      * @method post
-     * @param name:url type:int require:1 default:- other:- desc:图片url
-     * @param name:jumpurl type:int require:0 default:- other:- desc:跳转链接地址
-     * @param name:sort type:string require:1 default:- other:- desc:排序(越小越靠前)
-     * @param name:type type:string require:1 default:- other:- desc:类型_1首页轮播图_2保险页面图_3新闻页面轮播图
-     * @param name:status type:string require:1 default:- other:- desc:状态_1显示_2不显示
-     * @param name:newsid type:int require:0 default:- other:- desc:新闻id(type为3时)
+     * @param name:appkey type:string require:1 default:- other:- desc:短信appkey
+     * @param name:tid type:string require:0 default:- other:- desc:模板id
+     * @param name:code type:string require:1 default:- other:- desc:短信验证码变量
+     * @param name:service type:string require:1 default:- other:- desc:客服电话
+     * @param name:agreement type:string require:1 default:- other:- desc:协议
      * @author 开发者
      */
     public function save() {
-        $m = new \app\admin\model\note();
+        $m = new \app\admin\model\Note();
         $m->AddOne(request()->post());
     }
 
@@ -50,7 +49,7 @@ class Note extends Common {
         if ($id < 1) {
             show_json(0, '参数ID错误');
         }
-        $m = new \app\admin\model\note();
+        $m = new \app\admin\model\Note();
         $m->DelOne($id);
     }
 
@@ -70,7 +69,7 @@ class Note extends Common {
         if ($id < 1) {
             show_json(0, '参数ID错误');
         }
-        $m = new \app\admin\model\note();
+        $m = new \app\admin\model\Note();
         $m->EditOne($request->put(), $id);
     }
 
@@ -94,7 +93,7 @@ class Note extends Common {
         if ($id < 1) {
             show_json(0, '参数ID错误');
         }
-        $m = new \app\admin\model\note();
+        $m = new \app\admin\model\Note();
         $m->GetOne($id);
     }
 
--- application/admin/model/Note.php
+++ application/admin/model/Note.php
@@ -14,30 +14,27 @@ class Note extends Common {
 
     public function AddOne($params) {
         $data = array(
-            'url'        => trim($params['url']),
-            'jumpurl'    => trim($params['jumpurl']),
-            'sort'       => intval($params['sort']),
-            'type'       => intval($params['type']),
-            'status'     => intval($params['status']),
+            'appkey'     => trim($params['appkey']),
+            'tid'        => trim($params['tid']),
+            'code'       => intval($params['code']),
+            'service'    => intval($params['service']),
+            'agreement'  => intval($params['agreement']),
             'createtime' => time(),
         );
-        if (empty($data['url'])) {
-            show_json(0, '请传图片url');
+        if (empty($data['appkey'])) {
+            show_json(0, '请传短信appkey');
         }
-        if (empty($data['sort'])) {
-            show_json(0, '请传序号');
+        if (empty($data['tid'])) {
+            show_json(0, '请传模板id');
         }
-        if (empty($data['type'])) {
-            show_json(0, '请传图片类型');
+        if (empty($data['code'])) {
+            show_json(0, '请传短信验证码变量');
         }
-        if ($data['type'] == 3) {
-            if (empty($params['newsid'])) {
-                show_json(0, '请传新闻id');
-            }
-            $data['newsid'] = intval($params['newsid']);
+        if (empty($data['service'])) {
+            show_json(0, '请传客服电话');
         }
-        if (empty($data['status'])) {
-            $data['status'] == 1;
+        if (empty($data['agreement'])) {
+            show_json(0, '请传协议');
         }
         if ($this->data($data, true)->isUpdate(false)->save()) {
             //logs('创建新的??,ID:' . $this->getLastInsID(), 1);
