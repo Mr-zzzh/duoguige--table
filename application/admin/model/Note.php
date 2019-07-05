c8793a560428d7348efaceb517c77ede22b7c2f6
陈 <18627824375@163.com>
2019-07-05 14:25:19 +0800

    短信设置修改

--- application/admin/model/Note.php
+++ application/admin/model/Note.php
@@ -4,6 +4,38 @@ namespace app\admin\model;
 
 class Note extends Common {
 
+    public function AddOne($params) {
+        $data = array(
+            'appkey'     => trim($params['appkey']),
+            'tid'        => trim($params['tid']),
+            'code'       => trim($params['code']),
+            'service'    => trim($params['service']),
+            'agreement'  => trim($params['agreement']),
+            'createtime' => time(),
+        );
+        if (empty($data['appkey'])) {
+            show_json(0, '请传短信appkey');
+        }
+        if (empty($data['tid'])) {
+            show_json(0, '请传模板id');
+        }
+        if (empty($data['code'])) {
+            show_json(0, '请传短信验证码变量');
+        }
+        if (empty($data['service'])) {
+            show_json(0, '请传客服电话');
+        }
+        if (empty($data['agreement'])) {
+            show_json(0, '请传协议');
+        }
+        if ($this->data($data, true)->isUpdate(false)->save()) {
+            //logs('创建新的??,ID:' . $this->getLastInsID(), 1);
+            show_json(1, '添加成功');
+        } else {
+            show_json(0, '添加失败');
+        }
+    }
+
     public function EditOne($params, $id) {
         $data = array(
             'appkey'     => trim($params['appkey']),
@@ -36,11 +68,11 @@ class Note extends Common {
                 show_json(0, '添加失败');
             }
         } else {
-            if ($this->data($data, true)->isUpdate(false)->save()) {
-                //logs('创建新的??,ID:' . $this->getLastInsID(), 1);
-                show_json(1, '添加成功');
+            if ($this->save($data, array('id' => $id)) !== false) {
+                //logs('编辑??,ID:' . $id, 3);
+                show_json(1, '编辑成功');
             } else {
-                show_json(0, '添加失败');
+                show_json(0, '编辑失败');
             }
         }
     }
