47cf53dcf10d09b3425fbf9a270d335669d70818
zengzhihang <1095959430@qq.com>
2019-07-05 14:24:15 +0800

    短信接口设置--静态和交互

---
+++ public_html/yuntiqaq/src/components/news/newsxq.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        123
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '',
+        data() {
+            return {
+                id:'',
+            }
+        },
+        methods:{
+
+
+
+
+            getinfo(){
+                this.$axios({
+                    method:'get',
+                    url:`${this.api}admin/news/${this.id}`,
+                    params:{
+
+                    }
+                }).then(res=>{
+                    if(res.data.status == 1){
+                        console.log(res.data.data)
+                    }
+                })
+            },
+        },
+        created(){
+            this.id = this.$route.query.id
+            console.log(this.bjid == '')
+            if(this.bjid != ''){
+                this.getinfo()
+            }
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    
+</style>
