e8400e80655cf61e2aac0ab21d2f494efadd7648
陈 <18627824375@163.com>
2019-07-06 16:42:28 +0800

    修改

--- application/mobile/model/Area.php
+++ application/mobile/model/Area.php
@@ -7,7 +7,7 @@ class Area extends Common {
     public function pca_tree() {
         $tree = cache('pca_tree');
         if (empty($tree)) {
-            $list = $this->field('id,name,pid')->select()->toArray();
+            $list = $this->field('id,name,pid,code')->select()->toArray();
             $tree = list_to_tree($list);
             cache('pca_tree', $tree);
         }
