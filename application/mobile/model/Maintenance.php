8c722eee3376500a9457853f0baed2276b55d518
陈 <18627824375@163.com>
2019-07-09 14:28:29 +0800

    修改

--- application/mobile/model/Maintenance.php
+++ application/mobile/model/Maintenance.php
@@ -11,7 +11,7 @@ class Maintenance extends Common {
             if (intval($params['type']) == 1) {
                 $map['m.status'] = 0;
             } else {
-                $map['m.status'] = array('>', 1);
+                $map['m.status'] = array('>=', 1);
             }
         }
         if (!empty($params['genre'])) {
