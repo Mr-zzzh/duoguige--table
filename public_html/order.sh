8622be8ba08fd5d3dac3df4d255432e3bbefd39e
陈 <18627824375@163.com>
2019-07-29 08:49:26 +0800

    修改

--- public_html/order.sh
+++ public_html/order.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-count=`ps -ef|grep /www/wdlinux/phps/71/bin/php /www/web/yunti/public_html/index.php admin/Task/order >> /www/web/ccgg/public_html/order.log|grep -v grep|wc -l`
+count=`ps -ef|grep /www/wdlinux/phps/71/bin/php /www/web/yunti/public_html/index.php admin/Task/order >> /www/web/yunti/public_html/order.log|grep -v grep|wc -l`
 if [ ${count} -eq 0 ];then
-/www/wdlinux/phps/71/bin/php /www/web/yunti/public_html/index.php admin/Task/order >> /www/web/ccgg/public_html/order.log
+/www/wdlinux/phps/71/bin/php /www/web/yunti/public_html/index.php admin/Task/order >> /www/web/yunti/public_html/order.log
 fi
